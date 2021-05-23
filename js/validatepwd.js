f8a6eeccfc8f40a8032e02501cc03b34d775562e
Enxo8 <enzox844@gmail.com>
2021-05-24 00:13:04 +0200

    Cambio de nombres ultima hora

--- js/validatepwd.js
+++ js/validatepwd.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 		var pass1 = $('#pwd').val();
 		var pass2 = $('#pwd2').val();
-
+        
 		if ( pass1 == pass2 ) {
 			$('#error2').text("Coinciden").css("color", "black");
 		} else {
